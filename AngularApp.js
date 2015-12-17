4a389f73d9b5c9f3ef41ec3a2b4d2cadb3523c3c
BenReppert28 <benreppert28@gmail.com>
2015-12-17 13:59:20 -0600

    AngularApp.js
    The angularjs application.

---
+++ AngularApp.js
@@ -0,0 +1,53 @@
+(function(){
+  var app = angular.module('baseballSearch', []);
+  
+  app.controller("BaseballController", function(){
+	  this.products = searches;
+  });
+
+  app.controller("SearchController",function(){
+	 this.search = {}; 
+	 
+	 this.searchDB = function(baseball){
+		baseball.searches.push(this.search); 
+		this.baseball = {};
+	 };
+  });
+  
+  var searches = [
+  {
+	  name: 'playerID',
+	  year: 'yearID',
+	  gameNum: 'gameNum',
+	  gameID: 'gameID',
+	  team: 'teamID',
+	  lgID: 'lgID',
+	  GP: 'GP',
+	  startingPos: 'startingPos',
+  },
+  {
+	year: 'yearID',
+	team: 'teamID',
+	lgID: 'lgID',
+	player: 'playerID',
+	G_all: 'G_all',
+	GS: 'GS',
+	G_batting: 'G_batting',
+	G_defense: 'G_defense',
+	G_p: 'G_p',
+	G_c: 'G_c',
+	G_1b: 'G_1b',
+	G_2b: 'G_2b',
+	G_3b: 'G_3b',
+	G_ss: 'G_ss',
+	G_lf: 'G_lf',
+	G_cf: 'G_cf',
+	G_rf: 'G_rf',
+	G_of: 'G_of',
+	G_dh: 'G_dh',
+	G_ph: 'G_ph',
+	G_pr: 'G_pr',  
+  }
+  
+  ]
+})();
