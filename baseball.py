b63021f78d978f9d2104db31ea5bd262d229214f
BenReppert28 <benreppert28@gmail.com>
2015-12-18 14:59:45 -0600

    Update baseball.py

--- baseball.py
+++ baseball.py
@@ -1,18 +1,15 @@
 from pymongo import MongoClient
 import json
+app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
   return render_template('index.html')
 
 @app.route('/start', methods=['POST'])
-def get_counts():
-  #get url
-  data = json.loads(request.data.decode())
-  url = data["url"]
-  #form URL, id necessary
-  if 'http://' not in url[:7]:
-    url = 'http://' + url
+def get_results():
+  searchword = request.args.get('key', '')
+  
   #start job
   job = q.enqueue_call(
     func=count_and_save_words, args=(url,), result_ttl=5000
@@ -29,3 +26,8 @@ cursor = db.variable.find({<field1>:<value1>})#empty find() brings it all
 
 for document in cursor:
   print(document) #this iterates through the documents that are brought back
+
+if __name__ == '__main__':
+  app.run()
+
+main()
